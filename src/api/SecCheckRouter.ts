779cb41e8b8ac65fe9c1fa5bdbd3b4f619a1d47b
hm <mark.huang1212@gmail.com>
2021-04-22 17:46:46 +0800

    Add CLI helper files

--- src/api/SecCheckRouter.ts
+++ src/api/SecCheckRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import CVerifySector from "../cli/CVerifySector";
 
 const secCheckRouter = Router()
 
 secCheckRouter.get('/check/:sectorAfid', async (req, res) => {
+
+    const ret = await CVerifySector.shared.verifySector(
+        req.params.sectorAfid as string,
+        req.query.seed_hash as string)
+
     res.json({
-        result: true
+        result: ret
     })
+
 })
 
 export default secCheckRouter
