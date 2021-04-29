8c39e58d32a651da274ce88303f4b7b7d9df7d12
hm <mark.huang1212@gmail.com>
2021-04-29 19:45:14 +0800

    Remove deprecated

--- src/cli/CGetSector.test.ts
+++
@@ -1,9 +0,0 @@
-import CGetSector from "./CGetSector"
-
-test('Get All Sectors', async () => {
-    const ret = await CGetSector.shared.getAllSectors();
-    expect(ret.length).toBeGreaterThan(10);
-    for(const afid of ret){
-        expect(typeof afid).toBe('string')
-    }
-})
